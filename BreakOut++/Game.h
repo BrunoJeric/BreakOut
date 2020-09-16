3967dbe4df53ba748b25c9295e94c96264b9583c
JericBruno <Brunojeric@gmail.com>
2020-09-16 17:04:09 +0200

    game engine intitial setup

---
+++ BreakOut++/Game.h
@@ -0,0 +1,21 @@
+#pragma once
+#define SDL_MAIN_HANDLED
+#include "SDL.h"
+#include<iostream>
+
+class Game {
+public:
+	Game();
+	~Game();
+	void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+	bool running() { return isRunning; }
+private:
+	int br = 0;
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
